c6b8ff79530ac4d82769155d345bdb528dd509a8
juroga_new <juanjrgast@gmail.com>
2023-01-10 16:15:00 +0100

    Added #insert and #delete. Modified Tree, removed arr variable and substituted by @data

--- main.rb
+++ main.rb
@@ -11,11 +11,11 @@ class Node
 end
 
 class Tree
-    attr_accessor :node, :left, :right, :start, :finish, :mid
+    attr_accessor :node, :left, :right, :start, :finish, :mid, :root, :data
 
     def initialize(arr)
-        @arr = arr.sort!.uniq
-        @root = build_tree(@arr)
+        @data = arr.sort!.uniq
+        @root = build_tree(@data)
     end
 
     def build_tree(arr)
@@ -36,8 +36,61 @@ class Tree
         pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
 
+    def insert(num, node = @root)
+        if num == node.data
+            return node.data
+        else
+            if num < node.data
+                node.left.nil? ? node.left = Node.new(num) : insert(num, node.left)
+            else
+                node.right.nil? ? node.right = Node.new(num) : insert(num, node.right)
+            end
+        end
+    end
+
+    def delete(num, node = @root)
+        if node == nil
+            return node
+        end
+        if num > node.data
+            node.right = delete(num, node.right)
+        elsif num < node.data
+            node.left = delete(num, node.left)
+        else
+            if node.right.nil? && node.left.nil?
+                node = nil
+                return node
+            elsif node.right.nil?
+                temp = node.left
+                node = temp
+                node.left = nil
+                return temp
+            elsif node.left.nil?
+                temp = node.right
+                node = temp
+                node.right = nil
+                return temp
+            else
+                current_node = node.right
+                while current_node.left != nil do
+                    current_node = current_node.left
+                end
+                node.data = current_node.data
+                node.right = delete(current_node.data, node.right)
+            end
+        end
+        node
+    end
+
+
+
 end
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 bst = Tree.new(arr)
 bst.pretty_print
+p bst
+bst.delete(67)
+bst.pretty_print
+p bst
+
